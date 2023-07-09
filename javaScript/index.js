4fd0bcc59e3c340401f6d4b03f125380f17d31c4
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-07-09 11:58:51 -0700

    Update 0.3
    - added recent articles function

--- javaScript/index.js
+++ javaScript/index.js
@@ -9,18 +9,18 @@ const mainPageEl = document.getElementById('main-page-el')
 
 document.addEventListener("click",function(e) {
     if(e.target.dataset.blogId) {
-        renderFullBlog(e.target.dataset.blogId)
+        mainPageEl.innerHTML = renderFullBlog(e.target.dataset.blogId)
     }
 
 })
 
 //TODO: allow user to click on img to go to article. currently can only select text to
 
-function renderFeatureBlog() {
-    //renders the most recent blog to the top of the home page with custom styling
+function renderFeatureBlog() { //renders the most recent blog to the top of the home page with custom styling
+
     const addHTML = `
     <div class="header-text"data-blog-id= ${blogs[0].id}>
-        ${renderBlog(blogs[0])}
+        ${renderBlogCard(blogs[0])}
     </div>`
 
     featureBlogEl.style.background = `url(${blogs[0].img})`
@@ -29,29 +29,26 @@ function renderFeatureBlog() {
     featureBlogEl.innerHTML = addHTML
 }
 
-function renderBlogList() {
-    //renders the grid of blog posts
+function renderBlogCardList(cardNum) { //use argument to specify how many cards to render
     let addHTML = ""
+    console.log("called card list")
 
-
-    for(let i = 1; i < blogs.length; i++) { //starts at one to avoid render of the feature blog(most recent added)
-
+    for(let i = 1; i < cardNum; i++) { //starts at one to avoid render of the feature blog(most recent added)
+        console.log('adding html')
         addHTML += `
         <div class="blog" data-blog-id= ${blogs[i].id}>
             <img class="blog-img" data-blog-id= ${blogs[i].id} src="${blogs[i].img}" alt="blog cover image">
-            ${renderBlog(blogs[i])}
+            ${renderBlogCard(blogs[i])}
         </div>
-        
         `
     }
 
-    blogContainerEl.innerHTML = addHTML
+    return addHTML
 
 }
 
-function renderBlog(blog) {
-    //returns html for a specific blog
-    //not used for the full blog, only for home page
+function renderBlogCard(blog) { //returns html for a specific blog
+    
     const addHTML = `
         
         <p class="blog-info" data-blog-id= ${blog.id}>${blog.info}</p>
@@ -61,8 +58,9 @@ function renderBlog(blog) {
     return addHTML
 }
 
-function renderFullBlog(blogId) {
-    //used to render out the full blog on page without switching html files
+function renderFullBlog(blogId) { //used to render out the full blog on page without switching html files
+    
+    window.scrollTo(0,0)
     console.log(`render full article ${blogId}`)
     
     const blog = blogs.filter(function(blog){
@@ -76,14 +74,20 @@ function renderFullBlog(blogId) {
             <p class="fullBlog-paragraph">${blog.summery}</p>
             <img class="fullBlog-img" src="${blog.img}" alt="blog cover image">
             <p>${blog.content}</p>
-        </div>`
+            <a class="blog-link" target="_blank" href="${blog.link}">want to read more?</a>
+            <h2 class="fullBlog-title recent-posts">Recent posts</h2>
+        </div>
 
-    mainPageEl.innerHTML = addHTML
+        <section class="blogs-container" id="blog-container-el">${renderBlogCardList(3)}</section>
+        `
+
+    return addHTML
+    
 }
 
 function render() {
     renderFeatureBlog()
-    renderBlogList()
+    blogContainerEl.innerHTML = renderBlogCardList(blogs.length) //argument renders all cards
 }
 
 render()
